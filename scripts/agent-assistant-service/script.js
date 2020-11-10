67f1e381c85729e7f0341812be3ff3cf94447f58
armandsb1 <52469305+armandsb1@users.noreply.github.com>
2020-11-10 22:05:26 +0200

    Update script.js

--- scripts/agent-assistant-service/script.js
+++ scripts/agent-assistant-service/script.js
@@ -10,7 +10,8 @@ const responseMap = {
     3: "We're sorry to hear about your experience.",
     4: "We're glad that you enjoy our service!",
     5: "Goodbye. Please don't forget to like and subscribe to our channel.",
-    6: "Mums žēl, ka Jums radās problēmas", "aaa",
+    6: "Mums žēl, ka Jums radās problēmas", 
+    6: "aaaa",
     7: "Priecājamies, ka varējām Jums palīdzēt"
 }
 
